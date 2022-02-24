4afcd212075e6302c6af47c10c87360b61177f6f
shivam danawale <shivamdanawale@gmail.com>
2022-02-24 23:57:58 +0530

    Removed edit this page link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,14 +23,10 @@ const config = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -57,7 +53,7 @@ const config = {
           },
           { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/shivam1317/Cybernotes",
             position: "right",
             className: "header-github-link",
             "aria-label": "GitHub repository",
